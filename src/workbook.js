77db8a3e784a1b920d12cda025aee53c52dc5fdf
Borgar <borgar@borgar.net>
2021-11-09 16:35:46 +0000

    Support calculation properties

--- src/workbook.js
+++ src/workbook.js
@@ -1,4 +1,5 @@
 import attr from './utils/attr.js';
+import { toInt, toNum } from './utils/typecast.js';
 
 class Workbook {
   constructor () {
@@ -15,6 +16,9 @@ class Workbook {
       sheets: this.sheets,
       names: this.names
     };
+    if (this.calcProps) {
+      wb.calculation_properties = this.calcProps;
+    }
     if (!this.options.cell_styles) {
       wb.styles = this.styles;
     }
@@ -44,6 +48,18 @@ export default function (dom) {
 
   const pr = dom.querySelectorAll('workbook > workbookPr')[0];
   wb.epoch = (pr && +attr(pr, 'date1904')) ? 1904 : 1900;
+
+  const calcPr = dom.getElementsByTagName('calcPr')[0];
+  if (calcPr) {
+    const iterate = toInt(attr(calcPr, 'iterate'));
+    if (iterate && isFinite(iterate)) {
+      wb.calcProps = {
+        iterate: true,
+        iterate_count: toInt(attr(calcPr, 'iterateCount', 100)),
+        iterate_delta: toNum(attr(calcPr, 'iterateDelta', 0.001))
+      };
+    }
+  }
 
   return wb;
 }
