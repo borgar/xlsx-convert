b94ba36ae2c3fca3bb7826e01dc543555df36f81
Borgar <borgar@borgar.net>
2025-10-01 19:07:36 +0000

    Don't parse (relative) formulas until we absolutely need to.

--- src/RelativeFormula.ts
+++ src/RelativeFormula.ts
@@ -3,16 +3,18 @@ import { translateToA1, translateToR1C1 } from '@borgar/fx';
 export class RelativeFormula {
   anchorA1: string;
   formula: string;
-  relative: string;
+  relative: string | undefined;
 
   constructor (formula: string, anchorCell: string) {
     this.anchorA1 = anchorCell;
     this.formula = formula;
-    this.relative = translateToR1C1(formula, anchorCell) as string;
   }
 
   /** @param {string} offsetCell */
   translate (offsetCell: string): string {
+    if (!this.relative) {
+      this.relative = translateToR1C1(this.formula, this.anchorA1) as string;
+    }
     return translateToA1(this.relative, offsetCell) as string;
   }
 }
