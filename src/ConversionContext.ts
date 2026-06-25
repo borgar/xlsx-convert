8c39ec7eb15d71dab35c076b947d481f97ddec9b
Borgar <borgar@borgar.net>
2026-06-25 15:11:34 +0000

    Select defaults into a base theme based on workbookPr indicator (#60)
    The baseline theme can be determined from a property on workbookPr.
    `@jsfkit/utils` supplies all known themes so we can read the correct
    starting-point theme from that based on the
    workbookPr.defaultThemeVersion property if we got one.

--- src/ConversionContext.ts
+++ src/ConversionContext.ts
@@ -1,10 +1,9 @@
-import { INDEXED_COLORS } from '@jsfkit/utils';
+import { INDEXED_COLORS, THEMES } from '@jsfkit/utils';
 import type { Theme, DefinedName, External, Workbook } from '@jsfkit/types';
 import type { MetaData } from './handler/metadata.ts';
 import type { RDStruct } from './handler/rdstuct.ts';
 import type { RDValue } from './handler/rdvalue.ts';
 import type { Rel } from './handler/rels.ts';
-import { getBlankTheme } from './handler/theme.ts';
 import { DEFAULT_MDW } from './utils/mdw.ts';
 import type { RelativeFormula } from './RelativeFormula.ts';
 import type { ConversionOptions } from './index.ts';
@@ -64,6 +63,17 @@ export class ConversionContext {
   _arrayFormula?: string[];
   images: RefLink[];
   isLikelyGSExport: boolean;
+  /**
+   * Excel includes an undocumented number in the workbook properties that hints which
+   * default theme to use if a theme is not included. Values include:
+   * - 0 (or absent): no default theme override
+   * - 123820: Office 2007 theme (uses Calibri)
+   * - 124226: Office 2010 theme (uses Calibri)
+   * - 164011: Office 2013 theme (uses Calibri)
+   * - 166925: Office 2013–2022 theme (uses Calibri)
+   * - 202300: Microsoft 365 (2023+) theme (uses Aptos Narrow)
+   */
+  defaultThemeVersion: string;
   charts: RefLink[];
   /** Max Digit Width of the workbook Normal font, used to convert column widths to pixels. */
   normalMdw: number;
@@ -76,7 +86,8 @@ export class ConversionContext {
     this.rels = [];
     this.options = {};
     this.workbook = null;
-    this.theme = getBlankTheme();
+    this.defaultThemeVersion = '202300';
+    this.theme = THEMES.default;
     this.nameDefs = new Map();
     this.indexedColors = [ ...INDEXED_COLORS ];
     this.richStruct = [];
